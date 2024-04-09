f97ce19168ccc3be7ab36531961dbe57359e9c83
NGUYEN <truongchinguyen00@example.com>
2024-04-10 00:44:00 +0700

    Cài đặt api xử lý lỗi

--- app.js
+++ app.js
@@ -13,5 +13,15 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to MBB server!"});
 });
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
